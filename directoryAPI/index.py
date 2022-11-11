0869e840b0941de6f3ae4a64c1ebaa23e8eb985c
jbyeung <macbook@macbook-air.lan>
2022-11-10 23:47:54 -0500

    working on api

--- directoryAPI/index.py
+++ directoryAPI/index.py
@@ -1,37 +1,92 @@
 import os, stat
-from flask import Flask
+from flask import Flask, session
+from flask.json import jsonify
 
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+root = None
+cache = {}
+
+
+#------------------ routes
+
+#path for setting root directory and creating cache of all files
+@app.route("/<path:root_path>", methods = ['POST'])
+def set_root(root_path):
+    cwd = os.getcwd()
+    root_path = os.path.join(cwd, root_path)
+    if os.path.exists(root_path):
+        root = root_path
+
+        # read in everything in current and all sub directories into cache
+        cache = getContents(root)
+
+        session['root'] = root
+        session['cache'] = cache
+    else:
+        return "Bad root path"
+
+
+    return "200"
+
+
+#route for current directory (path is empty)
+@app.route("/", methods = ['GET'])
+def curr_directory():
+    root = session.get('root')
+    cache = session.get('cache')
+
+    if not root:
+        return "Need to set root directory first"
+
+    return jsonify(cache)
 
 #create REST endpoint for getting directories / files
-@app.route("/<scandir_path>", methods = ['GET'])
-def directory():
-    dir_list = os.scandir(path)
+@app.route("/<path:dir_path>", methods = ['GET'])
+def directory(dir_path):
+    root = session.get('root')
+    cache = session.get('cache')
+
+    if not root:
+        return "Need to set root directory first"
+
+    # walk down the path
+    path = dir_path.split("/")
+    res = cache
+    print(path)
+    for directory in path:
+        if directory in res:
+            res = res[directory]
+        else:
+            return "Bad path"
+
+    return jsonify(res)
+
+
+#---------------------------- helper functions
+
+def getContents(path):
+    # read in files and subdirectories
+    it = os.scandir(path)
     res = {}
 
-    # if requesting just a file, only return file contents
-    if len(dir_list) == 1 and dir_list.is_file():
-        entry = dir_list
-
-        # jsonify return
-        res[entry.name] = {
-            "contents" : readFile(scandir_path, entry),
-            "stats" : getStats(entry)
-        }
-    else:
-    #iterate through the files and directories
-        for entry in dir_list:
-            name = entry.name
-            if entry.is_dir():
-                name += "\\"
-
+    for entry in it:
+        name = entry.name
+        if entry.is_dir():
             res[name] = {
-                "stats" : getStats(entry)
+                "stats" : getStats(entry),
+                "contents" : getContents(os.path.join(path, entry.name))
             }
-        
-    
+        else:
+            res[name] = {
+                "stats" : getStats(entry),
+                "contents" : readFile(path, entry)
+            }
+
     return res
+            
 
 def getStats(entry):
     #returns the relevant stats on the entry requested
@@ -46,4 +101,9 @@ def getStats(entry):
 def readFile(path, entry):
     #reads file into string for json blob
     file_path = os.path.join(path, entry.name)
-    return open(path, 'r').read()
+    return open(file_path, 'r').read()
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
